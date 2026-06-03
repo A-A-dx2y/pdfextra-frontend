6b151c1f96a0bd9c853a24891e37ff87df4d92c5
achuasokan <achuasokan555@gmail.com>
2026-06-03 16:54:07 +0530

    removed

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -1,5 +1,5 @@
 import axios from 'axios';
-
+console.log("API BASE URL =", process.env.NEXT_PUBLIC_API_BASE_URL);
 const API_BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL;
 export const api = axios.create({
     baseURL: API_BASE_URL,
